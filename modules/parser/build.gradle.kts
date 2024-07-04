070da6d06fe0d81ab56124e5553c83283a8432ca
Roman Makeev <makeevrserg@gmail.com>
2024-07-04 15:56:32 +0300

    add parser shadowJar task

--- modules/parser/build.gradle.kts
+++ modules/parser/build.gradle.kts
@@ -1,7 +1,10 @@
+import ru.astrainteractive.gradleplugin.property.extension.ModelPropertyValueExt.requireProjectInfo
+
 plugins {
     id("org.jetbrains.kotlin.jvm")
     application
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.gradle.shadow)
 }
 
 dependencies {
@@ -22,4 +25,17 @@ dependencies {
 
 application {
     mainClass.set("com.flipperdevices.ifrmvp.parser.MainKt")
+}
+
+tasks.shadowJar {
+    isReproducibleFileOrder = true
+    mergeServiceFiles()
+    dependsOn(configurations)
+    archiveClassifier.set(null as String?)
+    from(sourceSets.main.get().output)
+    from(project.configurations.runtimeClasspath)
+
+    archiveVersion.set(requireProjectInfo.versionString)
+    archiveBaseName.set("${requireProjectInfo.name}-parser")
+    File(rootDir, "jars").also(File::mkdirs).also(destinationDirectory::set)
 }
