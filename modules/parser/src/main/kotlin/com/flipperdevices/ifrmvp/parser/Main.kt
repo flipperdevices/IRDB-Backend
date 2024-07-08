ba486e28979abdf279a7c1704575130c7260eff3
Roman Makeev <makeevrserg@gmail.com>
2024-07-08 12:11:29 +0300

    add mi remote database parser

--- modules/parser/src/main/kotlin/com/flipperdevices/ifrmvp/parser/Main.kt
+++ modules/parser/src/main/kotlin/com/flipperdevices/ifrmvp/parser/Main.kt
@@ -7,6 +7,10 @@ import com.flipperdevices.ifrmvp.parser.util.ParserPathResolver
 import kotlinx.coroutines.runBlocking
 
 fun main() {
+    runParser()
+}
+
+fun runParser() {
     val parserModule = ParserModule.Default(
         signalApiModule = SignalApiModule.Default(
             signalDbConnection = EnvKonfig.signalDatabaseConnection
@@ -39,3 +43,75 @@ fun mainUnused() {
             }
     }
 }
+
+fun fixMiRemoteDatabase() {
+    val miRemoteFolder = ParserPathResolver.irDbFolderFolder
+        .parentFile
+        .resolve("mi_remote_database")
+    val categories = miRemoteFolder.resolve("categories")
+    categories.listFiles()
+        .orEmpty()
+        .forEach { categoryFolder ->
+            categoryFolder.listFiles()
+                .orEmpty()
+                .filter { it.isDirectory }
+                .onEach { it.deleteRecursively() }
+
+            categoryFolder.listFiles()
+                .orEmpty()
+                .filter { !it.isDirectory }
+                .filter { it.extension == "ir" }
+                .forEach { irFile ->
+                    val brandName = irFile.name.let { name ->
+                        val splitByUnderscore = name.split("_")
+                        val i = splitByUnderscore.indexOfFirst { it.toIntOrNull() != null }
+                        if (i == -1) when {
+                            name.contains("kk") -> name.split("_kk").first()
+                            name.contains("mi") -> name.split("_mi").first()
+                            name.contains("mx") -> name.split("_mx").first()
+                            name.contains("xm") -> name.split("_xm").first()
+                            else -> splitByUnderscore.first()
+                        } else {
+                            splitByUnderscore.joinToString(separator = "_", limit = i, truncated = "").removeSuffix("_")
+                        }
+                    }
+                    val brandFolder = categoryFolder.resolve(brandName)
+                    if (!brandFolder.exists()) brandFolder.mkdir()
+
+                    val controlFolder = brandFolder.resolve(irFile.nameWithoutExtension)
+                    if (!controlFolder.exists()) controlFolder.mkdir()
+
+                    val controlFile = controlFolder.resolve(irFile.name)
+                    irFile.copyTo(controlFile)
+                    irFile.delete()
+                }
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
