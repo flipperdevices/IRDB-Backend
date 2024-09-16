69ddce17f443003a2fa1b9c14175a7c3962b7dbf
Roman Makeev <57789105+makeevrserg@users.noreply.github.com>
2024-09-16 21:56:58 +0300

    update kenerator speed by removing unnecessary logging (#10)

--- modules/build-konfig/src/main/kotlin/com/flipperdevices/ifrmvp/backend/envkonfig/KSystem.kt
+++ modules/build-konfig/src/main/kotlin/com/flipperdevices/ifrmvp/backend/envkonfig/KSystem.kt
@@ -1,11 +1,13 @@
 package com.flipperdevices.ifrmvp.backend.envkonfig
 
+import org.slf4j.LoggerFactory
+
 internal object KSystem {
-    private val logger = java.util.logging.Logger.getLogger("KSystem")
+    private val logger = LoggerFactory.getLogger("KSystem")!!
     fun getenvOrNull(key: String): String? {
         val value = System.getenv(key) ?: System.getProperty(key)
         if (value.isNullOrBlank()) {
-            logger.warning("Could not found ENV property: $key")
+            logger.warn("Could not found ENV property: $key")
             return null
         }
         return value
