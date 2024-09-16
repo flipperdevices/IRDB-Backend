497982044bc876c6a9220d2782120ccc7deee77a
Roman Makeev <makeevrserg@gmail.com>
2024-09-16 21:21:04 +0300

    fix identify by name

--- modules/kenerator/sql/src/main/kotlin/com/flipperdevices/ifrmvp/parser/presentation/FillerController.kt
+++ modules/kenerator/sql/src/main/kotlin/com/flipperdevices/ifrmvp/parser/presentation/FillerController.kt
@@ -164,7 +164,10 @@ internal class FillerController(private val database: Database) : CoroutineScope
                                         this[SignalKeyTable.type] = IfrKeyIdentifier.Empty.TYPE
                                     }
 
-                                    is IfrKeyIdentifier.Name -> error("Identifying by name is not possible!")
+                                    is IfrKeyIdentifier.Name -> {
+                                        this[SignalKeyTable.remoteKeyName] = keyIdentifier.name
+                                        this[SignalKeyTable.type] = IfrKeyIdentifier.Name.TYPE
+                                    }
 
                                     is IfrKeyIdentifier.Sha256 -> {
                                         this[SignalKeyTable.remoteKeyName] = keyIdentifier.name
@@ -191,7 +194,9 @@ internal class FillerController(private val database: Database) : CoroutineScope
                                                 andWhere { SignalTable.hash eq keyIdentifier.hash }
                                             }
 
-                                            is IfrKeyIdentifier.Name -> error("Identifying by name is not possible!")
+                                            is IfrKeyIdentifier.Name -> {
+                                                andWhere { SignalTable.name eq keyIdentifier.name }
+                                            }
                                         }
                                     }
                                     .map { it[SignalTable.id] }
